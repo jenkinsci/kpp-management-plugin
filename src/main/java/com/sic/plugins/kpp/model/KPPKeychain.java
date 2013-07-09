5159dcfa761f497eb6a3791f976cfce53ea6e447
Michael Bär <michael.baer@sic-software.com>
2013-07-09 18:17:35 +0200

    remove reading keystore from keychain

--- src/main/java/com/sic/plugins/kpp/model/KPPKeychain.java
+++ src/main/java/com/sic/plugins/kpp/model/KPPKeychain.java
@@ -6,18 +6,8 @@ import hudson.model.Describable;
 import hudson.model.Descriptor;
 import hudson.model.Hudson;
 import hudson.util.Secret;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.Serializable;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.cert.CertificateException;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.kohsuke.stapler.DataBoundConstructor;
 
@@ -148,47 +138,4 @@ public final class KPPKeychain implements Describable<KPPKeychain>, Serializable
             return "Keychain";
         }
     }
-    
-    /**
-     * This method loads the uplaoded keystore from a file.
-     *
-     * @return loaded Keystore
-     */
-    private static KeyStore loadKeystoreFromFile(String keyStoreFilePath, String keyStorePW) throws
-            KeyStoreException,
-            FileNotFoundException,
-            IOException,
-            NoSuchAlgorithmException,
-            CertificateException,
-            NoSuchProviderException {
- 
-        //KeyStore ks = KeyStore.getInstance("KeychainStore", "Apple");
-        //KeyStore ks = KeyStore.getInstance("KeychainStore");
-        KeyStore ks = KeyStore.getInstance("KeychainStore");
-
-        //keyStoreFilePath = "/Users/michaelbar/Documents/Projekte/SIC/Jenkins_ZPP_Plugin/source/kpp/work/kpp_upload/empty.keychain";
-        
-        keyStorePW = "123456";
-        char[] password = keyStorePW.toCharArray();
-        File keychainFile = new File(keyStoreFilePath);
-        LOGGER.log(Level.INFO, "content: {0}", keychainFile.toString());
-        if (!keychainFile.isFile()) {
-            if (keychainFile.isDirectory()) {
-                String[] files = keychainFile.list();
-                for (String filename : files) {
-                    LOGGER.log(Level.INFO, filename);
-                }
-            }
-        }
-
-        FileInputStream fis = new FileInputStream(keychainFile);
-        ks.load(fis, password);
-        fis.close();
-        
-        LOGGER.log(Level.INFO, "Provider1: {0}", ks.getProvider());
-        LOGGER.log(Level.INFO, "Provider2: {0}", ks.getProvider().toString());
- 
-        return ks;
-    }
-    
 }
